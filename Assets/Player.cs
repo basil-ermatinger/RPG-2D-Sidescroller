1037b1a7d9133fd0cac5a4e910dacc539674a5c8
Basil Ermatinger <62306996+Vrijl@users.noreply.github.com>
2025-08-05 22:11:54 +0200

    Implemented basic state machine

---
+++ Assets/Player.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public StateMachine stateMachine { get; private set; }
+
+	private EntityState idleState;
+
+	private void Awake()
+	{
+		stateMachine = new StateMachine();
+		
+		idleState = new EntityState(stateMachine, "Idle State"); // TODO: Magic String to enum
+	}
+
+	private void Start()
+	{
+		stateMachine.Initialize(idleState);
+	}
+
+	private void Update()
+	{
+		stateMachine.currentState.Update();
+	}
+}
