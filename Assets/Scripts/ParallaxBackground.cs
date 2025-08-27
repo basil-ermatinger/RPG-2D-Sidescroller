3d61868de1b15793e11b9e2edafa7ebb1ce3d82d
Basil Ermatinger <62306996+Vrijl@users.noreply.github.com>
2025-08-27 20:17:23 +0200

    Implemented functionality for endless background images.

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -6,23 +6,39 @@ namespace Rpg2dSidescroller
   {
     private Camera _mainCamera;
 		private float _lastCameraPositionX;
+		private float _cameraHalfWidth;
 
     [SerializeField] private ParallaxLayer[] _backgroundLayers;
 
 		private void Awake()
 		{
 			_mainCamera = Camera.main;
+			_cameraHalfWidth = _mainCamera.orthographicSize * _mainCamera.aspect;
+
+			CalculateBackgroundWidth();
 		}
 
-		private void Update()
+		private void FixedUpdate()
 		{
 			float currentCameraPositionX = _mainCamera.transform.position.x;
 			float distanceToMove = currentCameraPositionX - _lastCameraPositionX;
 			_lastCameraPositionX = currentCameraPositionX;
 
+			float cameraLeftEdge = currentCameraPositionX - _cameraHalfWidth;
+			float cameraRightEdge = currentCameraPositionX + _cameraHalfWidth;
+
 			foreach(ParallaxLayer layer in _backgroundLayers)
 			{
 				layer.Move(distanceToMove);
+				layer.LoopBackground(cameraLeftEdge, cameraRightEdge);
+			}
+		}
+
+		private void CalculateBackgroundWidth()
+		{
+			foreach(ParallaxLayer layer in _backgroundLayers)
+			{
+				layer.CalculcateImageWidth();
 			}
 		}
 	}
